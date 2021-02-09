9f1dd50382b43269c89883358f425eca8abf4168
Denis Belik <denis.belik2014@gmail.com>
2021-02-09 17:48:24 +0200

    add user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,6 +14,14 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        
+        $role_id = DB::table('user_role')->where('role', '=', "Admin")->select('id')->first()->id;
+
+        DB::table('users')->insert([
+            'email' => $_ENV["DB_SEEDER_USER_EMAIL"],
+            'password' => $_ENV["DB_SEEDER_USER_PASSWORD"],
+            'first_name' => 'Alabi',
+            'second_name' => 'Admin',
+            'role_id' => $role_id
+        ]);
     }
 }

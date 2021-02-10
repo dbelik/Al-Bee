36275aa1cee883c8f47ef1c5170f253c10be01e3
Denis Belik <denis.belik2014@gmail.com>
2021-02-10 16:02:03 +0200

    search for users from form

--- resources/js/Pages/SearchUsers.vue
+++ resources/js/Pages/SearchUsers.vue
@@ -7,18 +7,18 @@
         </template>
 
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="shadow flex mt-12">
-                <input class="w-full rounded p-2" type="text" placeholder="Search users by their names...">
-                <button class="bg-white w-auto flex justify-end items-center text-blue-500 p-2 hover:text-blue-400">
-                    <i class="material-icons">search</i>
+            <form v-on:submit="searchUsers" class="shadow flex mt-12">
+                <input v-model="search" class="w-full rounded p-2" type="text" placeholder="Search users by their names..." autofocus>
+                <button v-on:click="searchUsers" class="bg-white w-auto flex justify-end items-center text-blue-500 p-2 hover:text-blue-400">
+                    <i class="material-icons">Search</i>
                 </button>
-            </div>
+            </form>
         </div>
         
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-8 py-4">
-                    <ul>
+                    <ul v-if="users.length > 0">
                         <li v-for="user in users" class="flex items-center justify-between my-5">
                             <div class="flex items-center">
                                 <div 
@@ -29,6 +29,7 @@
                             <a :href="'/users/' + user.id">View</a>
                         </li>
                     </ul>
+                    <span v-else>Can't find anyone</span>
                 </div>
             </div>
         </div>
@@ -44,6 +45,18 @@
         },
         props: {
             users: Array
+        },
+        data() {
+            return {
+                search: ''
+            };
+        },
+        methods: {
+            searchUsers(event) {
+                event.preventDefault();
+                if (this.search)
+                    window.location.href = "users?search=" + this.search;
+            }
         }
     }
 </script>

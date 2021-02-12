d854189694161a2306066e78bda07d9a17971f22
Denis Belik <denis.belik2014@gmail.com>
2021-02-12 20:14:44 +0200

    make content container responsive

--- resources/js/Components/Containers/Content.vue
+++ resources/js/Components/Containers/Content.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="h-screen w-screen flex items-center justify-center">
-        <div class="bg-dark-blue-800 max-w-screen-xl max-h-content w-full h-full rounded-3xl shadow-2xl flex items-center justify-center flex-col">
+        <div class="bg-dark-blue-900 xl:bg-dark-blue-800 max-w-screen-xl max-h-screen xl:max-h-content w-full h-full rounded-none xl:rounded-3xl shadow-2xl flex items-center justify-center flex-col">
             <slot />
         </div>
     </div>

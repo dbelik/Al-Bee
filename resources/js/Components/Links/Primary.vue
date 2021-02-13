c13705a6003ba774fdfa45e139a48903496848d5
Denis Belik <denis.belik2014@gmail.com>
2021-02-13 22:08:35 +0200

    add first content to dashboard page

---
+++ resources/js/Components/Links/Primary.vue
@@ -0,0 +1,12 @@
+<template>
+    <inertia-link :href="href" class="text-primary hover:text-primary-interact focus:text-primary-interact duration-100">
+        <slot />
+    </inertia-link>
+</template>
+<script>
+export default {
+    props: {
+        href: String
+    },
+}
+</script>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,20 +1,18 @@
 <template>
     <app-layout>
-        <div v-if="user.role == 'admin'">
-            Hi admin
-        </div>
-        <div v-else>
-            Hi user
-        </div>
+        <h2>Hi, {{ user.first_name }} {{ user.second_name }}!</h2>
+        <h3 v-if="user.role === 'admin'">Do you wan't to <primary-link href="/users">search for users</primary-link>?</h3>
     </app-layout>
 </template>
 
 <script>
     import AppLayout from '@/Layouts/AppLayout'
+    import PrimaryLink from '@/Components/Links/Primary'
 
     export default {
         components: {
             AppLayout,
+            PrimaryLink
         },
 
         metaInfo: {
@@ -23,10 +21,6 @@
 
         props: {
             user: Object
-        },
-
-        mounted() {
-            console.log(this.user)
         },
     }
 </script>
